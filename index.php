56a30635ab46b9e941734ff8a4adbe7c388d3937
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-05-09 17:07:59 +0200

    feat: index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__."/config/constants.php";
 
 use Core\Container;
 use Core\Router;
