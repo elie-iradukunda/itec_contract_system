43119e04fef853561a02abeca7fcd17e5884fe27
Nziza Oscar <santosjuniorsoftwareengineer1@gmail.com>
2026-05-09 14:43:29 +0200

    feat: vendor/composer/autoload_real.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit0961b8e2a2e5ac6d59c46366b0d8f537
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit0961b8e2a2e5ac6d59c46366b0d8f537::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
