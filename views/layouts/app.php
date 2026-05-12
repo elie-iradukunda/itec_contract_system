af4655a85038db9ce39e6a8742cb0f6ed6c7a5e5
elieiradukunda <iradukundaelie71@gmail.com>
2026-05-12 17:42:17 +0200

    changing UI

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -21,6 +21,7 @@ $showPageHeader = $showPageHeader ?? isset($pageHeading);
     <title><?= ui_e($pageTitle) ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?= ui_asset('css/home.css') ?>?v=<?= ui_e($assetVersion) ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
